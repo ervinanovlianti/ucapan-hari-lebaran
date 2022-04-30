ac884e3fe55365a13a1d265eb6e3e59482cccb4c
ervinanovlianti <ervinanov03@gmail.com>
2022-04-30 22:25:09 +0800

    firs-commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const Iya = document.querySelector(".next");
+const Text = document.getElementById("text");
+const Image = document.querySelector("#image-gif");
+
+Iya.addEventListener("click", function () {
+    Iya.value = parseInt(Iya.value) + 1;
+    Text.classList.remove('text-animate');
+
+    if (Iya.value == 1) {
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Assalamualaikum 🤗";
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 2) {
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Dari Aku untuk Kamu 😀";
+        Image.setAttribute("src", "https://c.tenor.com/uygoMR-ATWgAAAAj/happy-ied-mubarak-ied-mubarak.gif");
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 3) {
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Kaki kadang salah langkah";
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 4) {
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Bibir kadang salah ucap";
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 5) {
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Hati kadang salah sangkah";
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 6) {
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Janji kadang terlupakan";
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 7) {
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Maka alangkah indahnya saling memaafkan";
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 8) {
+        Image.setAttribute("src", "https://c.tenor.com/T_16HTs34x4AAAAj/taqabbal-allahu-minna-wa-minkum-accept-god-with-us-and-you.gif");
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "تَقَبَّلَ اللَّهُ مِنَّا وَمِنْكُمْ وَ تَقَبَّلْ ياَ كَرِيْمُ <br> Taqabbalallahu Minna Wa Minkum";
+        Text.classList.add('text-animate');
+    }else if (Iya.value == 9) {
+        Image.setAttribute("src", "https://c.tenor.com/57JSPYRo0k8AAAAj/lebaran-selamat-lebaran.gif");
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Selamat hari raya Idul Fitri 1443 Hijriah <br> Mohon Maaf Lahir dan Batin 😁";
+        Text.classList.add('text-animate');
+        Iya.remove();
+    }
+});
